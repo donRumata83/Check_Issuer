4b7f503fee401d1ebb122981ddcae62708863cb6
donRumata <rokachov@gmail.com>
2017-07-20 10:47:43 +0300

    Structure commit 20.07.2017 Add Parser interface to Model pack

---
+++ src/Model/Paser.java
@@ -0,0 +1,12 @@
+package Model;
+
+import java.util.Date;
+import java.util.List;
+
+public interface Paser {
+    Cheque getLast();
+    List<Cheque> getForDate(Date from, Date to);
+    Cheque getByNumber();
+    List<Cheque> getByProductName(String name);
+
+}
--- src/Model/SQLParser.java
+++ src/Model/SQLParser.java
@@ -1,4 +1,26 @@
 package Model;
 
-public class SQLParser {
+import java.util.Date;
+import java.util.List;
+
+public class SQLParser implements Paser{
+    @Override
+    public Cheque getLast() {
+        return null;
+    }
+
+    @Override
+    public List<Cheque> getForDate(Date from, Date to) {
+        return null;
+    }
+
+    @Override
+    public Cheque getByNumber() {
+        return null;
+    }
+
+    @Override
+    public List<Cheque> getByProductName(String name) {
+        return null;
+    }
 }
